920a142b00be3515b949c31c09c7b8e405739fc3
FaithS15-6 <samaf0315@masseyhigh.school.nz>
2021-02-19 13:13:04 +1300

    Yes or no checker trial

---
+++ 00_sandbox.py
@@ -0,0 +1,9 @@
+# get name until and exit code is entered...
+
+name = ""
+while name.lower() != "xxx":
+      name = input("Who are you? ")
+      print(name)
+
+print ()
+print("We are done!")
---
+++ 01_Yes_No_Vol.1.py
@@ -0,0 +1,22 @@
+# Ask the user if they have played before
+show_instructions = input ("Have you played this game before? ").lower()
+
+
+# If they say yes, output 'Program continues'
+if show_instructions == "yes":
+    print("Program continues")
+
+elif show_instructions == "y":
+    print("program continues")
+
+elif show_instructions == "no":
+    print("display instructions")
+
+elif show_instructions == "no":
+    print("display instructions")
+
+# if they say no, output 'display instructions'
+else:
+    print("Please answer yes / no")
+
+print("You chose {}".format(show_instructions))
