2dfb33dd85b5121cbb84de10a225ceb1b09e3590
FaithS15-6 <samaf0315@masseyhigh.school.nz>
2021-03-05 13:13:34 +1300

    01 yes no trial

--- 01_Yes_No_Vol.1.py
+++ 01_Yes_No_Vol.1.py
@@ -1,22 +1,26 @@
-# Ask the user if they have played before
-show_instructions = input ("Have you played this game before? ").lower()
 
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before? ").lower()
 
-# if they say no, output 'display instructions'
-# If they say yes, output 'Program continues'
-if show_instructions == "yes":
-    print("Program continues")
+    # If they say yes, output 'Program continues'
+    # if they say no, output 'display instructions'
+    # If the answer is invalid, print an answer.
 
-elif show_instructions == "y":
-    print("program continues")
+    if show_instructions == "yes":
+        print("Program continues")
 
-elif show_instructions == "no":
-    print("display instructions")
+    elif show_instructions == "y":
+        print("program continues")
 
-elif show_instructions == "no":
-    print("display instructions")
+    elif show_instructions == "no":
+        print("display instructions")
 
-else:
-    print("Please answer yes / no")
+    elif show_instructions == "n":
+        print("display instructions")
 
-print("You chose {}".format(show_instructions))
+    else:
+        print("Please answer yes / no")
+
+    print("You chose {}".format(show_instructions))
