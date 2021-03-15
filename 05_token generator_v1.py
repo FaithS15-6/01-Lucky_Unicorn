3577a8792fdab586898496edf81a7eba5843536f
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-03-16 10:56:26 +1300

    Update 05_token generator_v1.py

--- 05_token generator_v1.py
+++ 05_token generator_v1.py
@@ -2,10 +2,20 @@ import random
 
 # main routine goes here
 
-tokens = ["unicorn" ,"horse", "donkey", "zebra"]
+tokens = ["unicorn", "horse", "donkey", "zebra"]
+balance = 100
 
 # Testing loop to generate 20 tokens
 for item in range(0,20):
     chosen = random.choice(tokens)
-    print(chosen, end='\t')
 
+    # Adjust balance
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+    # output
+    print("Token: {} , Balance: ${}".format(chosen, balance))
