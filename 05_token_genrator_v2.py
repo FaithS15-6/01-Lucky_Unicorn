fe1ceefcf0c35b8b0b44162fc5b54f238c0ef32a
FaithS15-6 <samaf0315@masseyhigh.school.nz>
2021-03-16 20:35:21 +1300

    05 token generator

--- 05_token_genrator_v2.py
+++ 05_token_genrator_v2.py
@@ -5,7 +5,7 @@ import random
 tokens = ["unicorn" ,"horse", "donkey", "zebra"]
 
 # Testing loop to generate 20 tokens
-for item in range(0,20):
+for item in range(0, 20):
     chosen = random.choice(tokens)
     print(chosen, end='\t')
 
