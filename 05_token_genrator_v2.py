8c46babcc84113c6ef72f6975679202b56136c21
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-03-18 14:18:19 +1300

    token generator

--- 05_token_genrator_v2.py
+++ 05_token_genrator_v2.py
@@ -2,7 +2,7 @@ import random
 
 # main routine goes here
 
-tokens = ["unicorn" ,"horse", "donkey", "zebra"]
+tokens = ["unicorn", "horse", "donkey", "zebra"]
 
 # Testing loop to generate 20 tokens
 for item in range(0, 20):
