d35b5582d228caf76cbe7f9819a256a80564142a
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-03-23 10:17:08 +1300

    05 token generator v4

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,5 +1,5 @@
 import random
 
-for item in range(0,20):
-      number = random.randit(0, 4)
-      print(number, end="/t")
+for item in range(0, 20):
+    number = random.randint(1, 4)
+    print(number, end="/t")
